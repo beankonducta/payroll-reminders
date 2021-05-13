c78f24439ebad5e4960e644b17571ddce10049fa
patrick <patrickxandrews@gmail.com>
2021-05-13 14:26:23 -0600

    make twilio send text reminders

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+var dotenv = require('dotenv').config()
+
 var dayjs = require('dayjs')
 var utc = require('dayjs/plugin/utc')
 var timezone = require('dayjs/plugin/timezone')
 dayjs.extend(utc);
 dayjs.extend(timezone);
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+// Numbers to send to
+const sendToNum = process.env.TWILIO_SEND_TO;
+const sendFromNum = process.env.TWILIO_SEND_FROM;
 
 // Specify timezone desired for date calculations
 dayjs.tz.setDefault("America/Denver");
@@ -13,9 +23,32 @@ const paydays = [1, 16];
 // Debug flag
 const debug = true;
 
+// Flag to disable notifications
+let payrollSubmitted = false;
+
 setInterval(() => {
-    console.log(isPayrollDay(dayjs()))
-}, 500);
+    const date = dayjs();
+    if (date.hour() % 2 === 0 && date.hour() > 9 && date.hour() < 16 && !payrollSubmitted) {
+        if (isPayrollDay(date)) {
+            client.messages
+                .create({
+                    body: `It's payroll day, DON'T FORGET TO SUBMIT PAYROLL! Reply 'done' when completed to silence reminders.`,
+                    from: sendFromNum,
+                    to: sendToNum
+                })
+                .then(message => console.log(`Payroll reminder sent to ${sendToNum}`));
+        }
+        if (isMonday(date)) {
+            client.messages
+                .create({
+                    body: `It's Monday, DON'T FORGET TO PAYOUT TIPS! Reply 'done' when completed to silence reminders.`,
+                    from: sendFromNum,
+                    to: sendToNum
+                })
+                .then(message => console.log(`Tipout reminder sent to ${sendToNum}`));
+        }
+    }
+}, 3600000); // once per hour
 
 let isMonday = (date) => {
     return date.day() === 1;
