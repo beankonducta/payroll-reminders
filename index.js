8b12974dd61318ca2b4d27e502a44d3e8d42ef74
patrick andrews <patrickxandrews@gmail.com>
2021-05-13 09:08:47 -0600

    make basic isPayrollDay check

--- index.js
+++ index.js
@@ -3,16 +3,31 @@ var utc = require('dayjs/plugin/utc')
 var timezone = require('dayjs/plugin/timezone')
 dayjs.extend(utc);
 dayjs.extend(timezone);
+
+// Specify timezone desired for date calculations
 dayjs.tz.setDefault("America/Denver");
 
+// Days to run payroll
+const paydays = [1, 16];
+
 setInterval(() => {
-    console.log(isMonday(dayjs()));
+    isPayrollDay(dayjs())
 }, 5000);
 
-// from a date string (new Date()) we can offset -6 hours to get to mountain time
 let isMonday = (date) => {
     return date.day() === 1;
 }
 
 let isPayrollDay = (date) => {
+    // Sunday through Saturday represented as 0-6
+    const dayOfWeek = date.day();
+
+    // Numerical day of the month
+    const dayOfMonth = date.date();
+
+    // dayjs object for tomorrow
+    const tomorrow = date.date(dayOfMonth + 1);
+    
+    // Basic check if tomorrow's date is payday -- returns true if tomorrow is in paydays array && tomorrow is Monday thru Thursday
+    if((tomorrow.date() === paydays[0] || tomorrow.date() === paydays[1]) && (tomorrow.day() > 0 && tomorrow.day() < 5)) return true;
 }
